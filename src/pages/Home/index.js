bfeb079f99652a772d5f2c19992c0067813704de
Hiran Gondim <hiran.g.b.silva@gmail.com>
2019-11-08 10:50:55 -0300

    Add react-hooks ao projeto.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { MdAddShoppingCart } from 'react-icons/md';
 
@@ -11,69 +10,57 @@ import * as CartActions from '../../store/modules/cart/actions';
 
 import { ProductList } from './styles';
 
-class Home extends Component {
-    state = {
-        products: [],
-    };
+export default function Home() {
+    const [products, setProducts] = useState([]);
+    const amount = useSelector(state =>
+        state.cart.reduce((sumAmount, product) => {
+            sumAmount[product.id] = product.amount;
+            return sumAmount;
+        }, {})
+    );
 
-    async componentDidMount() {
-        const response = await api.get('products');
+    const dispatch = useDispatch();
+    useEffect(() => {
+        async function loadProducts() {
+            const response = await api.get('products');
 
-        const data = response.data.map(product => ({
-            ...product,
-            priceFormatted: formatPrice(product.price),
-        }));
+            const data = response.data.map(product => ({
+                ...product,
+                priceFormatted: formatPrice(product.price),
+            }));
 
-        this.setState({ products: data });
+            setProducts(data);
+        }
+
+        loadProducts();
+    }, []);
+
+    function handleAddProduct(id) {
+        dispatch(CartActions.addToCartRequest(id));
     }
 
-    handleAddProduct = id => {
-        const { addToCartRequest } = this.props;
+    return (
+        <ProductList>
+            {products.map(product => (
+                <li key={product.id}>
+                    <img src={product.image} alt={product.title} />
 
-        addToCartRequest(id);
-    };
+                    <strong>{product.title}</strong>
+                    <span>{product.priceFormatted}</span>
 
-    render() {
-        const { products } = this.state;
-        const { amount } = this.props;
-        return (
-            <ProductList>
-                {products.map(product => (
-                    <li key={product.id}>
-                        <img src={product.image} alt={product.title} />
+                    <button
+                        type="button"
+                        onClick={() => handleAddProduct(product.id)}
+                    >
+                        <div>
+                            <MdAddShoppingCart size={16} color="#FFF" />{' '}
+                            {amount[product.id] || 0}
+                        </div>
 
-                        <strong>{product.title}</strong>
-                        <span>{product.priceFormatted}</span>
-
-                        <button
-                            type="button"
-                            onClick={() => this.handleAddProduct(product.id)}
-                        >
-                            <div>
-                                <MdAddShoppingCart size={16} color="#FFF" />{' '}
-                                {amount[product.id] || 0}
-                            </div>
-
-                            <span>ADICIONAR AO CARRINHO</span>
-                        </button>
-                    </li>
-                ))}
-            </ProductList>
-        );
-    }
+                        <span>ADICIONAR AO CARRINHO</span>
+                    </button>
+                </li>
+            ))}
+        </ProductList>
+    );
 }
-
-const mapStateToProps = state => ({
-    amount: state.cart.reduce((amount, product) => {
-        amount[product.id] = product.amount;
-        return amount;
-    }, {}),
-});
-
-const mapDispatchToProps = dispatch =>
-    bindActionCreators(CartActions, dispatch);
-
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(Home);
